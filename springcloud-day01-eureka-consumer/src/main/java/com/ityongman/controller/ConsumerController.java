6a7f4b0e56a889266fbe1201b4c999f89645a94c
zhongyong <zhongyong.sdz@dtyunxi.com>
2018-12-11 19:01:05 +0800

    ribbon总价熔断器功能

--- springcloud-day01-eureka-consumer/src/main/java/com/ityongman/controller/ConsumerController.java
+++ springcloud-day01-eureka-consumer/src/main/java/com/ityongman/controller/ConsumerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.ityongman.service.ComputeService;
+
 @RestController
 public class ConsumerController {
 	@Value("${service.provider.name}")
@@ -15,8 +17,16 @@ public class ConsumerController {
 	@Autowired
 	private RestTemplate restTemplate ;
 	
+	@Autowired
+	private ComputeService computeService ;
+	
 	@RequestMapping(value="/add", method=RequestMethod.GET)
 	public String add() {
 		return restTemplate.getForEntity(serviceProvider+"/add?a=10&b=20" , String.class).getBody();
+	}
+	
+	@RequestMapping(value="/add2", method=RequestMethod.GET)
+	public String add2() {
+		return computeService.addService();
 	}
 }
