df4ec06918b0dc31cda26915620996ef0923301f
zhongyong <zhongyong.sdz@dtyunxi.com>
2018-12-12 22:03:21 +0800

    添加注册客户端项目

---
+++ springcloud-day03-config-server-client/src/main/java/com/ityongman/controller/ConfigCenterController.java
@@ -0,0 +1,19 @@
+package com.ityongman.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigCenterController {
+	@Value("${from}")
+	private String from ;
+	
+	@RequestMapping(value="/from", method=RequestMethod.GET)
+	public String from() {
+		return this.from ;
+	}
+}
