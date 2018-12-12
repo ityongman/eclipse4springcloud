9ddb00b746dba1c7715672666a72c8dbc2b2ed94
ityongman <dunzeshe@gmail.com>
2018-12-13 01:04:35 +0800

    新增配置中心工程

--- springcloud-day03-config-server-client/src/main/java/com/ityongman/ConfigServerClientApplication.java
+++ springcloud-day03-config-server-client/src/main/java/com/ityongman/ConfigServerClientApplication.java
@@ -2,8 +2,10 @@ package com.ityongman;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerClientApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ConfigServerClientApplication.class)
